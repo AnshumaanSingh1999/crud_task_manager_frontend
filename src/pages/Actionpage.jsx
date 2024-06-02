6f82da895145a91362c0117018f9a993e1a0e838
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-06-02 16:08:53 +0530

    Update Actionpage.jsx

--- src/pages/Actionpage.jsx
+++ src/pages/Actionpage.jsx
@@ -10,9 +10,6 @@ const Actionpage=()=>{
 
    
 
-
-
-
     const fetchalltasks=async()=>{
         try{
           const res=await axios.get("http://localhost:8800/tasks")
@@ -29,11 +26,10 @@ const Actionpage=()=>{
       },[])
 
 
-   
-
     const handledeleteclick=async(id)=>{
         try{
-          const res=await axios.get("http://localhost:8800/deletetaskg/"+id)
+          const res=await axios.get("http://localhost:8800/deletetask/"+id)
+          window.location.reload()
         }
         catch(err){
           console.log(err)
@@ -41,10 +37,9 @@ const Actionpage=()=>{
     }
 
 
-
     return(
         <div className="container">
-            <h1>Task Manager {deletetask.taskid}</h1>
+            <h1>Task Manager</h1>
         <br></br>
         <div className="addtask">
             <div className="row">
@@ -57,7 +52,6 @@ const Actionpage=()=>{
             </div>
             </div>
         <hr></hr>
-
         <div className="tasks">
         <table className="table table-hover">
         <thead>
@@ -81,9 +75,6 @@ const Actionpage=()=>{
         </tbody>
         </table>
         </div>
-
-
-
         </div>
 
     )
